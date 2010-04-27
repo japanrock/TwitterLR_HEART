7f205fe8c4ccaded0e8a6db5bfba153047019715
福重 伸太朗 <japanrock@fukushige-shintarou-no-iMac.local>
2010-04-27 09:24:20 +0900

    インデント修正

--- lrheart_twitter.rb
+++ lrheart_twitter.rb
@@ -12,15 +12,15 @@ require "kconv"
 require File.dirname(__FILE__) + '/twitter_oauth'
 
 # Usage:
-# 1. このファイルと同じディレクトリに以下３つのファイルを設置します。
-# * twitter_oauth.rb
-# * http://github.com/japanrock/TwitterTools/blob/master/twitter_oauth.rb
-# * sercret_key.yml
-# * http://github.com/japanrock/TwitterTools/blob/master/secret_keys.yml.example
-# * lrheart.yml
-# * http://github.com/japanrock/TwitterLR_HEART/blob/master/lrheart.yml 
-# 2. このファイルを実行します。
-# ruby lrheart_twitter.rb
+#  1. このファイルと同じディレクトリに以下３つのファイルを設置します。
+#   * twitter_oauth.rb
+#   * http://github.com/japanrock/TwitterTools/blob/master/twitter_oauth.rb
+#   * sercret_key.yml
+#   * http://github.com/japanrock/TwitterTools/blob/master/secret_keys.yml.example
+#   * lrheart.yml
+#   * http://github.com/japanrock/TwitterLR_HEART/blob/master/lrheart.yml 
+#  2. このファイルを実行します。
+#   ruby lrheart_twitter.rb
 
 class LrHeart
   attr_reader :selected_culture
